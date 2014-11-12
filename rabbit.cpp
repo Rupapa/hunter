ff5c9204acf7df85f81aa7053fd44f06fe9e203f
Rupapa <xxxxcc600@gmail.com>
2014-11-13 01:09:12 +0900

    Update rabbit.cpp

--- rabbit.cpp
+++ rabbit.cpp
@@ -1,61 +1,50 @@
-class Rabbit
-{
-  private:
-  int xpos, ypos;
-  int lifetime;
-  static int allRabbit, deadRabbit;
-  
-  public:
-  void move();
-  void breed();
+Rabbit::Rabbit()
+{	
+	srand(time(NULL));
+	xpos = rand()%20;
+	ypos = rand()%20;
+	lifetime = 0;
 }
 
-void Rabbit::move
+void Rabbit::move()
 {
-  int n;
-  srand(seed(NULL));
-check:  
-  n = rand()%4;
-  
-  switch(n)
-  {
-    case 0:
-    {
-      if (xpos==0)
-      goto check;
-      else
-      xpos--;
-    }break;
-    case 1:
-    {
-      if(xpos==19)
-      goto check;
-      else
-      xpos++;
-    }break;
-    case 2:
-    {
-      if(ypos==0)
-      goto check;
-      else
-      ypos--;
-    }break;
-    case 3:
-    {
-      if(ypos==20)
-      goto check;
-      else
-      ypos++;
-    }break;
-  }
+beginning:
+	srand(time(NULL));
+	int i = rand()%4;
+
+	switch(i)
+	{
+		case 0:
+		{
+			if(xpos==0)
+				goto beginning;
+			else
+				xpos--;
+		}break;
+		case 1:
+		{
+			if(xpos==19)
+				goto beginning;
+			else
+				xpos++;
+		}break;
+		case 2:
+		{
+			if(ypos==0)
+				goto beginning;
+			else
+				ypos--;
+		}break;
+		case 3:
+		{
+		  if(ypos==19)
+		    goto beginning;
+		  else
+		    ypos++;
+		}break;
+	}
 }
 
-void Rabbit::starve()
-{
-  if(starveTime==2)
-    delete this;
-    return;
-}
 Rabbit* Rabbit::breed()
 {
   if(lifetime%3==2)
