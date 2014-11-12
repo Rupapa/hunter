14cb67f77cd0cbbcb440c538fda54d71be303e09
Rupapa <xxxxcc600@gmail.com>
2014-11-12 22:10:36 +0900

    Update rabbit.h

--- rabbit.h
+++ rabbit.h
@@ -12,18 +12,46 @@ private:
 	char c;
 
 public:
-		Rabbit(int x, int y, int life)
-			:xpos(x), ypos(y), lifetime(life), c('R')	{}
+		Rabbit();
 		void move();
 		void breed();
 }
 
+Rabbit::Rabbit()
+{	
+	srand(time(NULL));
+	xpos = rand()%20;
+	ypos = rand()%20;
+	lifetime = 0;
+}
+
 void Rabbit::move()
 {
+beginning:
 	srand(time(NULL));
 	int i = rand()%4;
 
 	switch(i)
 	{
-		case 0: xpos++;
-		case 1: xpos--;
+		case 0:
+		{
+			if(xpos==0)
+				goto beginning;
+			else
+				xpos--;
+		}break;
+		case 1:
+		{
+			if(xpos==19)
+				goto beginning;
+			else
+				xpos++;
+		}break;
+		case 2:
+		{
+			if(ypos==0)
+				goto beginning;
+			else
+				ypos--;
+				
+		}
