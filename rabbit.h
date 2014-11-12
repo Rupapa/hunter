eebc5aff297224e9c398da2523f988ae23d67dbe
Rupapa <xxxxcc600@gmail.com>
2014-11-13 01:06:31 +0900

    Update rabbit.h

--- rabbit.h
+++ rabbit.h
@@ -1,57 +1,15 @@
 #ifndef RABBIT_H
 #define RABBIT_H
 
-#include<iostream>
-#include<cstdlib>
-using namespace std;
 class Rabbit
 {
-private:
-	int xpos, ypos;
-	int lifetime;
-	char c;
-
-public:
-		Rabbit();
-		void move();
-		void breed();
+  private:
+  int xpos, ypos;
+  int lifetime;
+  static int allRabbit, deadRabbit;
+  
+  public:
+  void move();
+  void breed();
 }
-
-Rabbit::Rabbit()
-{	
-	srand(time(NULL));
-	xpos = rand()%20;
-	ypos = rand()%20;
-	lifetime = 0;
-}
-
-void Rabbit::move()
-{
-beginning:
-	srand(time(NULL));
-	int i = rand()%4;
-
-	switch(i)
-	{
-		case 0:
-		{
-			if(xpos==0)
-				goto beginning;
-			else
-				xpos--;
-		}break;
-		case 1:
-		{
-			if(xpos==19)
-				goto beginning;
-			else
-				xpos++;
-		}break;
-		case 2:
-		{
-			if(ypos==0)
-				goto beginning;
-			else
-				ypos--;
-				
-		}
+#endnif
