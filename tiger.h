d393aa1e43be107ca22afed56f2f40e3e6338480
Rupapa <xxxxcc600@gmail.com>
2014-11-13 14:37:29 +0900

    Update tiger.h

--- tiger.h
+++ tiger.h
@@ -1,7 +1,7 @@
 #ifndef TIGER_H
 #define TIGER_H
 
-class Rabbit
+class Tiger
 {
   private:
   int xpos, ypos;
@@ -11,6 +11,31 @@ class Rabbit
   public:
   void move();
   Tiger* breed();
+  int detect_rabbit(char* field[20][20]);
+  void hunt_rabbit(int pos, Rabbit* r);
   void starve();
 }
 #endnif
+
+int Tiger::detect_rabbit(char* field[20][20])
+{
+  if(field[xpos-1][ypos]=='R')
+    return 0;
+  else if(field[xpos+1][ypos]=='R')
+    return 1;
+  else if(field[xpos][ypos-1]=='R')
+    return 2;
+  else if(field[xpos][ypos+1]=='R')
+    return 3;
+  else
+    return 4;
+}
+
+void hunt_rabbit(int pos, Rabbit* r)
+
+
+Rabbit* Tiger::findRabbitAdress(Rabbit* r ,char* field[20][20])
+{
+  for(int i=0; i<r_all; i++)
+    if(r->xpos==this->xpos || r->ypos==
+}
