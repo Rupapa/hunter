a7f87c1eb02f0e4d0b0dd5fda15d564ca066c38a
Rupapa <xxxxcc600@gmail.com>
2014-11-16 02:10:22 +0900

    Update tiger.h

--- tiger.h
+++ tiger.h
@@ -3,39 +3,23 @@
 
 class Tiger
 {
-  private:
-  int xpos, ypos;
-  int lifetime;
-  static int allTiger, allDeadTiger;
+	private:
+	int xpos, ypos;
+	int lifetime;
+	static int t_all, t_dead;
+	int isStrong;	//ignore!
   
-  public:
-  void move();
-  Tiger* breed();
-  int detect_rabbit(char* field[20][20]);
-  void hunt_rabbit(int pos, Rabbit* r);
-  void starve();
-}
-#endnif
+	public:
+	void move();
+	void starve();
+	Tiger* breed();
 
-int Tiger::detect_rabbit(char* field[20][20])
+	int detect(char* field[20][20]);
+	void hunt_rabbit(int pos, Rabbit* r);
+	
+}
+#endif
+
+int detect(char* field[20][20])
 {
-  if(field[xpos-1][ypos]=='R')
-    return 0;
-  else if(field[xpos+1][ypos]=='R')
-    return 1;
-  else if(field[xpos][ypos-1]=='R')
-    return 2;
-  else if(field[xpos][ypos+1]=='R')
-    return 3;
-  else
-    return 4;
-}
-
-void hunt_rabbit(int pos, Rabbit* r)
-
-
-Rabbit* Tiger::findRabbitAdress(Rabbit* r ,char* field[20][20])
-{
-  for(int i=0; i<r_all; i++)
-    if(r->xpos==this->xpos || r->ypos==
-}
+	
